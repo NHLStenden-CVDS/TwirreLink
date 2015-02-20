d0b5b4ed485a99496784972eaf7cc86e3d1183bb
Jasper Westra <jasper.westra@live.nl>
2015-02-20 11:56:19 +0100

    init commit appel

---
+++ src/SerialRW.h
@@ -0,0 +1,35 @@
+/*
+ * SerialRW.h
+ *
+ *  Created on: Dec 20, 2012
+ *      Author: jasper
+ */
+
+#ifndef SERIALRW_H_
+#define SERIALRW_H_
+
+class SerialRW
+{
+public:
+	SerialRW(char *serialPort, int baud);
+	~SerialRW();
+	int _fd;
+	int Initialize(char *serialPort, int baud);
+	unsigned char readByte();
+	std::string readString();
+	void readNBytes(unsigned char *buf, int n);
+	short readShort();
+	unsigned short readUShort();
+	void writeBytes(unsigned char *bytes, int nrOfBytes);
+	int writeByte(unsigned char byte);
+	void writeShort(short nShort);
+	void writeUShort(unsigned uShort);
+	void flush();
+	short _twoBytesToShort(unsigned char msb, unsigned char lsb);
+	float _fourBytesToFloat(unsigned char b1, unsigned char b2, unsigned char b3, unsigned char b4);
+private:
+	int bytesToRead;
+	void _changeNrOfBytesNeeded(int nrOfBytesNeeded);
+};
+
+#endif /* SERIALRW_H_ */
