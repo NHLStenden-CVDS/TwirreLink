eb9a11c3b8795b694be6fae7b43a3a45cda975d6
Martin Dijkstra <martin.dijkstra@nhl.nl>
2017-03-17 11:54:07 +0100

    add missing headers to install script

--- install-x86.sh
+++ install-x86.sh
@@ -9,10 +9,12 @@ mkdir $INC_DIR
 mkdir $INC_DIR/Core
 mkdir $INC_DIR/TwirreSerial
 mkdir $INC_DIR/Serial
+mkdir $INC_DIR/Logger
 
 cp x86/libtwirrelink.so $LIB_DIR/
 cp *.h $INC_DIR/
 cp Core/*.h $INC_DIR/Core/
+cp Logger/*.h $INC_DIR/Logger/
 cp TwirreSerial/TwirreSerial.h $INC_DIR/TwirreSerial/
 cp Serial/SerialRW.h $INC_DIR/Serial/
 
